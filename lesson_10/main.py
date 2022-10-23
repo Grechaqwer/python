46ba9a427a011b086a5424961146b4bb81cfc953
movavi_school <uvukoska@gmail.com>
2022-10-23 12:49:13 +0700

    23 окт

---
+++ lesson_10/main.py
@@ -0,0 +1,21 @@
+# for
+# lst = ["Антон", "Вова", "Гриша", "Олег", "Ян"]
+# for element in lst: # для каждого элемента lst
+#     print(element)
+#
+#
+# for i in range(0, 10):
+#     print(i)
+#
+# while
+is_game = True
+while is_game:
+    i = input("да/нет")
+    if i == "нет":
+        is_game = False
+
+    print("sue")
+
+
+
+
