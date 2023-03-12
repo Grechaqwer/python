664e4c523a311ec630591b8abb48ccbde0153303
movavi_school <uvukoska@gmail.com>
2023-03-12 13:15:53 +0700

    vc

---
+++ н/хх.py
@@ -0,0 +1,34 @@
+#  # import json# a = open("file.json", "w", encoding="utf-8")
+# # content = json.dump("Приффки", a, ensure_ascii=False)
+# # a.close()
+#
+#
+#  # class Person:
+#  #     def __init__(self, imya): # магический метод для инициализации
+#  #         self.mishurik = imya
+#  #         self.status = "Учитель"
+#  #     def __str__(self):
+#  #         return f"{self.mishurik} {self.status}"
+#  #
+#  # chelove = Person("Ы")
+#  #
+#  # print(chelove.mishurik)
+#  # print(str(chelove))
+#
+# class Iaga:
+#     def __init__(self, baba,surnam, ag):
+#         self.name = baba
+#         self.surname = surnam
+#         self.age = ag
+#     def __str__(self):
+#         return  f"Имя:{self.name},\n Фамилия:{self.surname}, \n Возвраст:{self.age}"
+#     def greet(self):
+#         return  f"Привет!Меня зовут {self.name} {self.surname}, и мне {self.age} лет"
+# tank = Iaga("Zina", "Egorova", 40)
+# print(tank.name, tank.surname, tank.age)
+# print(tank)
+# print(tank.greet())
+
+
+
+
